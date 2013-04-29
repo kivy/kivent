8e3471e20f1461c686fff7ee76e4d85d1f728d7f
Kovak <kovac1066@gmail.com>
2013-04-28 22:16:28 -0600

    Added quad renderer

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class GameWorld(Widget):
     number_entities = NumericProperty(0)
     systems = DictProperty({})
     num_children = NumericProperty(0)
+    last_touch = ListProperty((0,0))
 
     def __init__(self, **kwargs):
         super(GameWorld, self).__init__(**kwargs)
@@ -23,9 +24,10 @@ class GameWorld(Widget):
         self.deactivated_entities = []
         Clock.schedule_once(self.test_entity, 2.0)
         Clock.schedule_once(self.test_entity, 3.0)
-        #Clock.schedule_once(self.test_remove_system, 3.5)
-        #Clock.schedule_interval(self.update, .05)
         Clock.schedule_once(self.test_clear_entities, 3.5)
+        Clock.schedule_once(self.test_entity, 3.8)
+        Clock.schedule_once(self.test_entity, 3.8)
+        #Clock.schedule_interval(self.update, .03)
 
     def test_clear_entities(self, dt):
         self.clear_entities()
@@ -36,12 +38,31 @@ class GameWorld(Widget):
     def test_entity(self, dt):
         rand_x = random.randint(0, self.width)
         rand_y = random.randint(0, self.height)
-        create_component_dict = {'position': {'x': rand_x, 'y': rand_y}, 'position_renderer': {'texture': 'star1.png', 'render': True}}
+        create_component_dict = {'position': {'x': rand_x, 'y': rand_y}, 
+        'position_renderer': {'texture': 'star1.png', 'render': True}}
         component_order = ['position', 'position_renderer']
         self.init_entity(create_component_dict, component_order)
 
     def test_remove_entity(self, dt):
         self.remove_entity(0)
+
+    def on_camera_pos(self, instance, value):
+        systems = self.systems
+        for system in systems:
+            if systems[system].renderable:
+                systems[system].update(.01)
+
+    def on_touch_down(self, touch):
+        self.last_touch = touch.x, touch.y
+
+    def on_touch_move(self, touch):
+        last_touch = self.last_touch
+        dist_x = touch.x - last_touch[0]
+        dist_y = touch.y - last_touch[1]
+        self.camera_pos[0] += dist_x
+        self.camera_pos[1] += dist_y
+        self.last_touch = touch.x, touch.y
+
 
     def create_entity(self):
         entity = {'id': self.number_entities}
@@ -58,7 +79,6 @@ class GameWorld(Widget):
         self.entities[entity_id]['entity_load_order'] = component_order
         for component in component_order:
             systems[component].create_component(entity_id, components_to_use[component])
-        print self.entities[entity_id]
         
     def remove_entity(self, entity_id):
         entity = self.entities[entity_id]
@@ -71,7 +91,6 @@ class GameWorld(Widget):
                 components_to_delete.append(data)
             else:
                 components_to_delete.append(data)
-                print entity_id
                 systems[data].remove_entity(entity_id)
         for component in components_to_delete:
             del entity[component]
@@ -163,19 +182,40 @@ class BasicPositionSystem(GameSystem):
     def __init__(self, **kwargs):
         super(BasicPositionSystem, self).__init__(**kwargs)
 
-class BasicRenderSystem(GameSystem):
+class QuadRenderer(GameSystem):
     system_id = StringProperty('position_renderer')
     render_information_from = StringProperty('position')
+    context_information_from = StringProperty(None)
+    off_screen_pos = ListProperty((-100, -100))
     updateable = BooleanProperty(True)
     renderable = BooleanProperty(True)
+    do_rotate = BooleanProperty(False)
+    do_color = BooleanProperty(False)
+    do_scale = BooleanProperty(False)
+    image_mode = StringProperty(None)
+    textures = DictProperty(None)
 
     def __init__(self, **kwargs):
-        super(BasicRenderSystem, self).__init__(**kwargs)
+        super(QuadRenderer, self).__init__(**kwargs)
 
     def generate_component_data(self, entity_component_dict):
         entity_component_dict['translate'] = None
         entity_component_dict['quad'] = None
+        entity_component_dict['size'] = None
+        if self.do_rotate:
+            entity_component_dict['rotate'] = None
+        if self.do_color:
+            entity_component_dict['color'] = None
+        if self.do_scale:
+            entity_component_dict['scale'] = None
         return entity_component_dict
+
+    def load_texture(self, texture_str):
+        textures = self.textures
+        if texture_str not in textures:
+            textures[texture_str] = CoreImage(texture_str).texture
+        texture = textures[texture_str]
+        return texture
 
     def generate_entity_component_dict(self, entity_id):
         entity = self.parent.entities[entity_id]
@@ -184,26 +224,101 @@ class BasicRenderSystem(GameSystem):
         return entity_component_dict
 
     def draw_entity(self, entity_id):
-        entity = self.parent.entities[entity_id]
+        parent = self.parent
+        entity = parent.entities[entity_id]
         system_data = entity[self.system_id]
-        position = entity[self.render_information_from]
-        texture = CoreImage(system_data['texture']).texture
+        render_information = entity[self.render_information_from]
+        if self.do_scale or self.do_color:
+            context_information = entity[self.context_information_from]
+        texture = self.load_texture(system_data['texture'])
         size = texture.size[0] * .5, texture.size[1] *.5
+        system_data['size'] = size
+        camera_pos = parent.camera_pos
+        
         with self.canvas:
             PushMatrix()
             system_data['translate'] = Translate()
+            if self.do_rotate:
+                system_data['rotate'] = Rotate()
+                system_data['rotate'].angle = render_information['angle']
+            if self.do_scale:
+                system_data['scale'] = Scale()
+                system_data['scale'].x = context_information['scale_x']
+                system_data['scale'].y = context_information['scale_y']
+            if self.do_color:
+                system_data['color'] = Color()
+                system_date['color'].rgba = context_information['color']
             system_data['quad'] = Quad(texture = texture, points = (-size[0], -size[1], size[0], -size[1],
                 size[0], size[1], -size[0], size[1]))
-            system_data['translate'].xy = position['x'], position['y']
+            system_data['translate'].xy = render_information['x'] + camera_pos[0], render_information['y'] + camera_pos[1]
             PopMatrix()
+
+    def update_render_state(self):
+        parent = self.parent
+        camera_pos = parent.camera_pos
+        camera_size = parent.size
+        pos = self.pos
+        system_id = self.system_id
+        entities = parent.entities
+        for entity_id in self.entity_ids:
+            entity = entities[entity_id]
+            system_data = entity[system_id]
+            render_information = entity[self.render_information_from]
+            if render_information['x'] + system_data['size'][0] > pos[0] - camera_pos[0] and render_information['x'] - system_data['size'][0] < pos[0] - camera_pos[0] + camera_size[0]:
+                if render_information['y'] + system_data['size'][1] > pos[1] - camera_pos[1] and render_information['y'] - system_data['size'][1] < pos[1] - camera_pos[1] + camera_size[1]:
+                    if not system_data['render']:
+                        system_data['render'] = True
+                else:
+                    if system_data['render']:
+                        system_data['render'] = False
+            else:
+                if system_data['render']:
+                    system_data['render'] = False
+
+    def render(self):
+        parent = self.parent
+        entities = parent.entities
+        system_id = self.system_id
+        do_color = self.do_color
+        do_scale = self.do_scale
+        do_rotate = self.do_rotate
+        off_screen_pos = self.off_screen_pos
+        camera_pos = parent.camera_pos
+        render_information_from = self.render_information_from
+        if do_scale or do_color:
+            context_information_from = self.context_information_from
+        for entity_id in self.entity_ids:
+            entity = entities[entity_id]
+            system_data = entity[system_id]
+            print system_data['render'], entity_id
+            if system_data['render']:
+                render_information = entity[render_information_from]
+                if do_scale or do_color:
+                    context_information = entity[context_information_from]
+                system_data['translate'].xy = render_information['x'] + camera_pos[0], render_information['y'] + camera_pos[1]
+                if do_rotate:
+                    system_data['rotate'].angle = render_information['angle']
+                if do_scale:
+                    system_data['scale'].x = context_information['scale_x']
+                    system_data['scale'].y = context_information['scale_y']
+                if do_color:
+                    system_data['color'].rgba = context_information['color']
+            else:
+                if not system_data['translate'].x == off_screen_pos[0]:
+                    system_data['translate'].xy = off_screen_pos
+
+            
+    def update(self, dt):
+        self.update_render_state()
+        self.render()
+
 
     def remove_entity(self, entity_id):
         system_data = self.parent.entities[entity_id][self.system_id]
         for data in system_data:
             if isinstance(system_data[data], Instruction):
-                print 'removing instruction', data
                 self.canvas.remove(system_data[data])
-        super(BasicRenderSystem, self).remove_entity(entity_id)
+        super(QuadRenderer, self).remove_entity(entity_id)
 
 class KivEntApp(App):
     def build(self):

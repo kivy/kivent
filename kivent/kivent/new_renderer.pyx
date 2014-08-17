3e1a6a3c1d882bb81f30687f249bcb293db1c691
Kovak <kovac1066@gmail.com>
2014-08-17 00:51:14 -0600

    added RenderBatch first draft

--- kivent/kivent/new_renderer.pyx
+++ kivent/kivent/new_renderer.pyx
@@ -3,6 +3,109 @@ cdef extern from "string.h":
     void *memcpy(void *dest, void *src, size_t n)
 
 
+cdef class RenderBatch:
+    cdef list _entity_ids
+    cdef int _vert_count
+    cdef dict _entity_counts
+    cdef int _maximum_verts
+    cdef float* _batch_data
+    cdef unsigned short* _batch_indices
+    cdef int _index_count
+    cdef int _r_index_count
+    cdef int _r_vert_count
+    cdef int _attrib_count
+    cdef CMesh _cmesh
+
+
+    def __cinit__(self, int maximum_verts, int attribute_count, CMesh cmesh):
+        self._entity_ids = []
+        self._entity_counts = {}
+        self._maximum_verts = maximum_verts
+        self._batch_data = NULL
+        self._batch_indices = NULL
+        self._r_index_count = 0
+        self._r_vert_count = 0
+        self._vert_count = 0
+        self._index_count = 0
+        self._attrib_count = attribute_count
+        self._cmesh = cmesh
+
+    def update_batch(self):
+        cdef int vert_count = self._vert_count
+        cdef int r_vert_count = self._r_vert_count
+        cdef int index_count = self._index_count
+        cdef int r_index_count = self._r_index_count
+        cdef float* batch_data = self._batch_data
+        cdef int attribute_count = self._attrib_count
+        cdef CMesh cmesh = self._cmesh
+        cdef unsigned short* batch_indices = self._batch_indices
+        something_updated = False
+        if vert_count != r_vert_count:
+            if not batch_data:
+                batch_data = <float *>PyMem_Malloc(
+                    vert_count * attribute_count * sizeof(float))
+            else:
+                batch_data = <float *>PyMem_Realloc(batch_data, 
+                    attribute_count * vert_count * sizeof(float))
+                if not batch_data:
+                    raise MemoryError()
+            self._r_vert_count = vert_count
+            self._batch_data = batch_data
+            cmesh._vertices = batch_data
+            cmesh.vcount = vert_count
+            something_updated = True
+        if index_count != r_index_count:
+            if not batch_indices:
+                batch_indices = <unsigned short*>PyMem_Malloc(
+                    index_count * sizeof(unsigned short))
+            else:
+                batch_indices = <unsigned short*>PyMem_Realloc(
+                    batch_indices, index_count * sizeof(unsigned short))
+                if not batch_indices:
+                    raise MemoryError()
+            self._r_index_count = index_count
+            self._batch_indices = batch_indices
+            cmesh._indices = batch_indices
+            cmesh.icount = index_count
+            something_updated = True
+        if something_updated:
+            cmesh.flag_update()
+
+    def __dealloc__(self):
+        if self._batch_data != NULL:
+            PyMem_Free(self._batch_data)
+        if self._batch_indices != NULL:
+            PyMem_Free(self._batch_indices)
+
+    def add_entity(self, int entity_id, int num_verts, int num_indices):
+        if num_verts + self._vert_count > self._maximum_verts:
+            return False
+        else:
+            self._vert_count += num_verts
+            self._index_count += num_indices
+            self._entity_ids.append(entity_id)
+            self._entity_counts[entity_id] = (num_verts, num_indices)
+            return True
+
+    def remove_entity(self, int entity_id):
+        self._entity_ids.remove(entity_id)
+        entity_counts = self._entity_counts
+        num_verts, num_indices = entity_counts[entity_id]
+        self._vert_count -= num_verts
+        self._index_count -= num_indices
+        del entity_counts[entity_id]
+
+    def update_entity_counts(self, int entity_id, int new_vert_count, 
+        int new_indices_count):
+        entity_counts = self._entity_counts
+        num_verts, num_indices = entity_counts[entity_id]
+        vert_change = new_vert_count - num_verts
+        index_change = new_indices_count - num_indices
+        self._vert_count += vert_change
+        self._index_count += index_change
+        entity_counts[entity_id] = (new_vert_count, new_indices_count)
+
+
 cdef class NVertMesh:
     cdef int _attrib_count
     cdef float* _data
@@ -241,3 +344,18 @@ cdef class NVertMesh:
             start = attrib_count * i
             index = start + attribute_n
             data[index] = data[index] * value
+
+    def set_rectangle_mesh(self, float width, float height, list uvs):
+        self.vertex_count = 4
+        self.index_count = 6
+        self.indices = [0, 1, 2, 2, 3, 0]
+        w = .5*width
+        h = .5*height
+        u0, v0, u1, v1 = uvs
+        self[0] = [-w, -h, u0, v0]
+        self[1] = [-w, h, u0, v1]
+        self[2] = [w, h, u1, v1]
+        self[3] = [w, -h, u1, v0]
+
+
+

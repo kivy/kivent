74f492ff23b821dbb8a46d33a72f703cf86e9550
Kovak <kovac1066@gmail.com>
2015-05-01 16:36:31 -0600

    remove leftovers from old version

--- kivent/__init__.py
+++
@@ -1,7 +0,0 @@
-import kivent_core
-cmesh = kivent_core.cmesh
-renderers = kivent_core.renderers
-gameworld = kivent_core.gameworld
-gamesystems = kivent_core.gamesystems
-gamescreens = kivent_core.gamescreens
-import kivent_cymunk

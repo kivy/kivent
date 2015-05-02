2e1cbb19f5ccafe42438e4e49ef10f1da9be20e6
Kovak <kovac1066@gmail.com>
2015-05-01 21:58:50 -0600

    remove unused imports from older project structure

--- modules/core/kivent_core/__init__.py
+++ modules/core/kivent_core/__init__.py
@@ -1,14 +1,3 @@
-# import cmesh
-# import entity
-# import cwidget
-# import resource_managers
-# import vertmesh
-# #import gamesystems
-# #import gameworld
-# #import renderers
-# import gamescreens
-# import membuffer
-# import system_manager
 from kivy.core.window import Window
 import memory_handlers
 import rendering

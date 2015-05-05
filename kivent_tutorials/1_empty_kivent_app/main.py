bb1cef865a108fa48d27f299cab237e256ac305e
Kovak <kovac1066@gmail.com>
2015-05-05 15:30:04 -0600

    move/rename and cleanup empty apps

--- kivent_tutorials/1_empty_kivent_app/main.py
+++ kivent_tutorials/1_empty_kivent_app/main.py
@@ -2,25 +2,16 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 import kivent_core
-from kivy.clock import Clock
 
 
 class TestGame(Widget):
     def __init__(self, **kwargs):
         super(TestGame, self).__init__(**kwargs)
-        Clock.schedule_once(self.init_game)
+        self.gameworld.init_gameworld([], callback=self.init_game)
 
-    def init_game(self, dt):
-        try:
-            self.setup_map()
-            self.setup_states()
-            self.set_state()
-        except:
-            Clock.schedule_once(self.init_game)
-
-    def setup_map(self):
-        gameworld = self.gameworld
-        gameworld.currentmap = gameworld.systems['map']
+    def init_game(self):
+        self.setup_states()
+        self.set_state()
 
     def setup_states(self):
         self.gameworld.add_state(state_name='main', 
@@ -28,10 +19,9 @@ class TestGame(Widget):
             systems_removed=[], systems_paused=[],
             systems_unpaused=[],
             screenmanager_screen='main')
-   
+
     def set_state(self):
         self.gameworld.state = 'main'
-
 
 
 class YourAppNameApp(App):

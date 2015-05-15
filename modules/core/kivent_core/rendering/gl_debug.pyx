0b511c940e4203812aaa0d0cf33d21d12ab4cb8d
Kovak <kovac1066@gmail.com>
2015-05-14 18:10:09 -0600

    fix bug in string formatting for gl_log_debug_message

--- modules/core/kivent_core/rendering/gl_debug.pyx
+++ modules/core/kivent_core/rendering/gl_debug.pyx
@@ -4,4 +4,4 @@ from kivy.logger import Logger
 cdef void gl_log_debug_message(str name):
     cdef GLenum ret = glGetError()
     if ret:
-        Logger.error('Error in %s' % name,"OpenGL Error %d / %x" % (ret, ret))
+        Logger.error("OpenGL Error (%s) %d / %x" % (name, ret, ret))

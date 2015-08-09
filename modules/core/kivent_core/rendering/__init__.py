306c224b3eb8b8881652c9d0cfbb188227427112
Kovak <kovac1066@gmail.com>
2015-08-09 14:07:10 -0600

    remove old vertmesh, has been replaced by model

--- modules/core/kivent_core/rendering/__init__.py
+++ modules/core/kivent_core/rendering/__init__.py
@@ -1,7 +1,6 @@
 from . import vertex_format
 from . import frame_objects
 from . import cmesh
-from . import vertmesh
 from . import batching
 from . import vertex_formats
 from . import fixedvbo
--- modules/core/kivent_core/rendering/vertmesh.pxd
+++
@@ -1,6 +0,0 @@
-cdef class VertMesh:
-    cdef int _attrib_count
-    cdef float* _data
-    cdef int _vert_count
-    cdef int _index_count
-    cdef unsigned short* _indices

1431cc4666f3e1eed947578e9c84e9af41f1550b
Kovak <kovac1066@gmail.com>
2015-08-09 17:49:34 -0600

    testing rename further

--- modules/particles/kivent_particles/__init__.py
+++ modules/particles/kivent_particles/__init__.py
@@ -1,4 +1,4 @@
 from kivent_particles import particle
 from kivent_particles import emitter
-from kivent_particles import renderers
-from kivent_particles import vf
+from kivent_particles import particle_renderers
+from kivent_particles import particle_formats

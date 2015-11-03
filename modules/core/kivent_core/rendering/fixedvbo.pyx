236ca3bb73287b2d9677b808471f24572421a0c9
Kovak <kovac1066@gmail.com>
2015-11-03 01:12:04 -0700

    fix memory leak with buffer not being deleted

--- modules/core/kivent_core/rendering/fixedvbo.pyx
+++ modules/core/kivent_core/rendering/fixedvbo.pyx
@@ -2,7 +2,7 @@
 from kivy.graphics.context cimport Context, get_context
 from kivy.graphics.c_opengl cimport (GL_ARRAY_BUFFER, GL_STREAM_DRAW,
     GL_ELEMENT_ARRAY_BUFFER, glGenBuffers, glBindBuffer, glBufferData,
-    glBufferSubData)
+    glBufferSubData, glDeleteBuffers)
 from kivent_core.rendering.gl_debug cimport gl_log_debug_message
 from kivent_core.memory_handlers.block cimport MemoryBlock
 from vertex_format cimport KEVertexFormat
@@ -166,6 +166,7 @@ cdef class FixedVBO:
         and clear the **memory_block**.'''
         self.flags = V_NEEDGEN
         self.size_last_frame = 0
+        glDeleteBuffers(1, &self.id)
         if self.target == GL_ELEMENT_ARRAY_BUFFER:
             self.data_size = 0
         self.memory_block.clear()

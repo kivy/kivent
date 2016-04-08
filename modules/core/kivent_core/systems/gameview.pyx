ff0e7fc5531f456ef106466c78f2ebf08d2c0616
Meet Udeshi <mudeshi1209@gmail.com>
2016-04-09 04:14:27 +0530

    update_render_state fix

--- modules/core/kivent_core/systems/gameview.pyx
+++ modules/core/kivent_core/systems/gameview.pyx
@@ -109,8 +109,8 @@ cdef class GameView(GameSystem):
         Used internally by gameview to update the projection matrix to properly
         reflect the settings for camera_size, camera_pos, and the pos and size
         of gameview.'''
-        camera_pos = self.camera_pos
-        camera_size = self.window_size
+        camera_pos = self._rotate_point(self.camera_pos, self.camera_rotate)
+        camera_size = self.size
         x, y = self.pos
         camera_scale = self.camera_scale
         proj = Matrix()

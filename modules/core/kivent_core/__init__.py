9700c9e84e2707265e59e1b488b7e6fef2c7a54e
labuzm <labuzm@gmail.com>
2016-04-25 23:41:33 +0200

    consistent way of importing packages

--- modules/core/kivent_core/__init__.py
+++ modules/core/kivent_core/__init__.py
@@ -4,9 +4,9 @@ __VERSION__ = '2.1.0'
 
 if 'KIVENT_PREVENT_INIT' not in os.environ:
     from kivy.core.window import Window
-    from . import memory_handlers
-    from . import rendering
-    from . import managers
-    from . import entity
-    from . import gameworld
-    from . import systems
+    from kivent_core import memory_handlers
+    from kivent_core import rendering
+    from kivent_core import managers
+    from kivent_core import entity
+    from kivent_core import gameworld
+    from kivent_core import systems
--- modules/cymunk/kivent_cymunk/__init__.py
+++ modules/cymunk/kivent_cymunk/__init__.py
@@ -3,5 +3,5 @@ import os
 __VERSION__ = '1.0.0'
 
 if 'KIVENT_PREVENT_INIT' not in os.environ:
-    from . import physics
-    from . import interaction
+    from kivent_cymunk import physics
+    from kivent_cymunk import interaction

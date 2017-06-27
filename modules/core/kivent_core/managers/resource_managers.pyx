9fd0969ae8de27a6be9cc7b994a413f76b630f76
kovak <kovac1066@gmail.com>
2017-06-27 11:26:07 -0700

    fix for element_id None loading

--- modules/core/kivent_core/managers/resource_managers.pyx
+++ modules/core/kivent_core/managers/resource_managers.pyx
@@ -263,9 +263,13 @@ cdef class ModelManager(GameManager):
             + element_id for the SVGModelInfo.
 
         '''
+        if info.element_id is not None:
+            name = svg_name + '_' + info.element_id
+        else:
+            name = svg_name + '_None'
         model_key = self.load_model(
             'vertex_format_2f4ub', info.vertex_count, 
-            info.index_count, svg_name + '_' + info.element_id,
+            info.index_count, name,
             indices=info.indices, vertices=info.vertices,
             )
         self._svg_index[svg_name]['models'][info.element_id] = model_key

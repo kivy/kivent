1c6f5cdff17d86880509a124baafdb1b605027fe
maho <maho@pagema.net>
2017-08-30 02:11:09 +0200

    - new kind of shape, way better as example, code cleanup

--- examples/17_joints/main.py
+++ examples/17_joints/main.py
@@ -1,5 +1,6 @@
 from random import randint, choice
 from math import radians, pi, sin, cos
+from itertools import cycle
 from functools import partial
 from os.path import dirname, join, abspath
 
@@ -23,6 +24,8 @@ def get_asset_path(asset, asset_loc):
 
 texture_manager.load_image('redcircle.png')
 texture_manager.load_image('greencircle.png')
+texture_manager.load_image('circlewoffset.png')
+texture_manager.load_image('roundrect.png')
 texture_manager.load_image(get_asset_path('blue-tile.png', 'assets'))
 
 
@@ -43,6 +46,8 @@ class TestGame(Widget):
         phy.space.set_default_collision_handler(begin=self.on_default_collision)
 
         self.draw_floor()
+
+        self.createbody = iter(cycle([self.create_roundrect2body, self.create_1body, self.create_2body]))
 
     def draw_floor(self):
         self.gameworld.init_entity(
@@ -71,13 +76,89 @@ class TestGame(Widget):
     def draw_some_stuff(self):
         size = Window.size
         w, h = size[0], size[1]
-        delete_time = 2.5
-        create_asteroid = self.create_asteroid
         pos = (randint(0, w), randint(0, h))
-        ent_id = create_asteroid(pos)
-        self.app.count += 100
 
-    def create_asteroid(self, pos):
+        fn = next(self.createbody)
+        fn(pos)
+
+    def create_roundrect2body(self, pos):
+        w = 100
+        mass = 50
+        col_shapes = []
+        for x in (-w/4, w/4):
+            for y in (-w/4, w/4):
+                col_shapes.append({
+                                'shape_type': 'circle', 
+                                 'elasticity': .0, 
+                                 'collision_type': 1, 
+                                 'shape_info': {'inner_radius': 0, 
+                                                'outer_radius': w/4, 
+                                                'mass': mass/4, 
+                                                'offset': (x,y)}, 
+                                 'friction': 1.0
+                                })
+        physics_component = {'main_shape': 'circle', 
+                             'velocity': (0, 0), 
+                             'position': pos, 
+                             'angle': 0, 
+                             'angular_velocity': 0, 
+                             'vel_limit': 250, 
+                             'ang_vel_limit': radians(200), 
+                             'mass': mass, 
+                             'col_shapes': col_shapes}
+        create_component_dict = {'cymunk_physics': physics_component, 
+                                 'rotate_renderer': {'texture': 'roundrect', 
+                                                     'size': (w, w),
+                                                     'render': True}, 
+                                                     'position': pos, 'rotate': 0, 
+                                }
+        component_order = ['position', 'rotate', 'rotate_renderer', 
+                           'cymunk_physics',]
+        eid = self.gameworld.init_entity(create_component_dict, component_order)
+
+        posx, posy = pos
+        pos2 = posx, posy - w
+
+        e2 = self.create_body(pos2, 20, 'redcircle', mass=100, collision_type=0)
+
+        body1 = self.gameworld.entities[eid].cymunk_physics.body
+        body2 = self.gameworld.entities[e2].cymunk_physics.body
+
+        pivot = PivotJoint(body1, body2, pos2)
+        self.gameworld.physics.space.add(pivot)
+
+    def create_1body(self, pos):
+        """ create one body with center of gravity below center of circle """
+        radius = 90
+        mass = 100
+        shape_dict = {'inner_radius': 0, 
+                       'outer_radius': radius, 
+                      'mass': mass, 'offset': (0, -radius)}
+        col_shape = {'shape_type': 'circle', 
+                     'elasticity': .0, 
+                     'collision_type': 1, 
+                     'shape_info': shape_dict, 
+                     'friction': 1.0}
+        physics_component = {'main_shape': 'circle', 
+                             'velocity': (0, 0), 
+                             'position': pos, 
+                             'angle': 0, 
+                             'angular_velocity': 0, 
+                             'vel_limit': 250, 
+                             'ang_vel_limit': radians(200), 
+                             'mass': mass, 
+                             'col_shapes': [col_shape]}
+        create_component_dict = {'cymunk_physics': physics_component, 
+                                 'rotate_renderer': {'texture': 'circlewoffset', 
+                                                     'size': (2*radius, 4*radius),
+                                                     'render': True}, 
+                                                     'position': pos, 'rotate': 0, 
+                                }
+        component_order = ['position', 'rotate', 'rotate_renderer', 
+                           'cymunk_physics',]
+        return self.gameworld.init_entity(create_component_dict, component_order)
+
+    def create_2body(self, pos):
         e1 = self.create_body(pos, 100, 'greencircle', mass=10)
         posx, posy = pos
         pos2 = posx, posy - 125
@@ -91,10 +172,10 @@ class TestGame(Widget):
 
     def create_body(self, pos, radius, texture, mass, collision_type=1):
         shape_dict = {'inner_radius': 0, 'outer_radius': radius, 
-            'mass': mass, 'offset': (0, 0)}
-        col_shape = {'shape_type': 'circle', 'elasticity': .5, 
+            'mass': mass, 'offset': (0, 10)}
+        col_shape = {'shape_type': 'circle', 'elasticity': -0.5, 
             'collision_type': collision_type, 
-            'shape_info': shape_dict, 'friction': 1.0}
+            'shape_info': shape_dict, 'friction': 1.5}
         col_shapes = [col_shape]
         physics_component = {'main_shape': 'circle', 
             'velocity': (0, 0), 
